8fb4861b9de07389ac548ab3435c7fd3f6253b4c
Nishita Pande <89330962+nishitapande@users.noreply.github.com>
2024-06-17 08:51:10 +0530

    added links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,7 +25,7 @@ function Projects() {
               title="CuisineConnect"
               description="CuisineConnect is a dynamic web application built using the MERN stack, combining the power of MongoDB, Express.js, React, and Node.js. This user-friendly platform allows food enthusiasts to search for and create delicious recipes. With an intuitive search feature, users can easily discover a variety of recipes to suit their culinary preferences."
               ghLink="https://github.com/nishitapande/recipe-share-frontend"
-              // demoLink="https://chatify-49.web.app/"
+              demoLink="https://recipe-share-frontend.vercel.app/"
             />
           </Col>
 
@@ -48,7 +48,7 @@ function Projects() {
               title="Landing Page Clone"
               description="I developed a clone of the Crest.ai landing page using React. This project replicates the sleek and modern design of the original site, showcasing my proficiency in front-end development with React. The clone includes responsive design elements, interactive features, and a clean, user-friendly interface."
               ghLink="https://github.com/nishitapande/landingPage-internship-assignment"
-              // demoLink="https://editor.soumya-jit.tech/"
+              demoLink="https://landing-page-internship-assignment.vercel.app/"
             />
           </Col>
 
@@ -59,6 +59,7 @@ function Projects() {
               title="Landing page for OmniFoods"
               description="Landing page for Omnifood using HTML, CSS, and JavaScript, incorporating EJS (Embedded JavaScript) for templating. This project showcases a modern and responsive design, offering an engaging user experience with dynamic content rendered efficiently using EJS. The landing page features attractive layouts, smooth animations, and interactive elements, demonstrating my skills in front-end development and templating with EJS. This project underscores my ability to build visually appealing and functional web pages with a focus on user experience."
               ghLink="https://github.com/nishitapande/omnifood"
+              demoLink="https://omnifood-front-end-project.netlify.app/"
             />
           </Col>
         </Row>
